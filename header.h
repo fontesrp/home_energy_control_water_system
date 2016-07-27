1dc272bc9a6ec2dea548d4774971fff415138dc5
fontesrp <fontesr2@gmail.com>
2016-07-27 12:34:23 -0300

    Update main files to correctly use the project's functions. The setup function will initialize all pins and set the device's clock. In case the clock can't be set (energy failure), it won't let the program continue and, since all pins are initialized LOW, all circuits will stay ON. Once the clock is set, the loop function will get the circuits' scheduled state for the current time and request that all pins be brought to that state. It than checks every one second if at least one minute has past before repeating its entire process.

--- header.h
+++ header.h
@@ -4,6 +4,9 @@
 #include <Time.h>
 #include <TimeLib.h>
 
-void setDeviceTime();
+void setDeviceTime(); // device_clock.ino
+char getScheduledState(int currentWeekday, int currentHour, int currentMinute); // schedule.ino
+void initializePins(); // pin_state.ino
+void updatePinState(char desiredState); // pin_state.ino
 
 #endif
--- home_energy_control_water_system.ino
+++ home_energy_control_water_system.ino
@@ -1,12 +1,37 @@
 #include "header.h"
 
 void setup() {
+
 	Serial.begin(9600);
-	setDeviceTime();
+
+	initializePins();
+
+	while (timeStatus() == timeNotSet) {
+		setDeviceTime();
+	}
 }
 
 void loop() {
 
-	// Your code here
+	time_t checkTime;
+	int checkWeekday, checkHour, checkMinute;
+	char scheduledState;
 
+	checkTime = now();
+
+	checkWeekday = weekday(checkTime);
+	checkHour = hour(checkTime);
+	checkMinute = minute(checkTime);
+
+	scheduledState = getScheduledState(checkWeekday, checkHour, checkMinute);
+
+	if (scheduledState == -1) {
+		Serial.println("Error: invalid scheduledState");
+	} else {
+		updatePinState(scheduledState);
+	}
+
+	while (checkMinute == minute()) {
+		delay(1000);
+	}
 }
